3ac6eaf11d48328322e354e3f2b6cf708204a722
STIWARTs <stiwartsaxena477@gmail.com>
2025-09-03 00:05:56 +0530

    P17

--- Robotics/Arduino Uno Rev3/Bluetooth_module_with_servo_motor/bluetooth_servo.ino
+++ Robotics/Arduino Uno Rev3/Bluetooth_module_with_servo_motor/bluetooth_servo.ino
@@ -1,5 +1,14 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ https://www.arduino.cc/en/Tutorial/LibraryExamples/Sweep
+*/
+
 #include <Servo.h>
-char angle;
+char angle; //bluetooth value save
 
 Servo myservo;  // create Servo object to control a servo
 // twelve Servo objects can be created on most boards
@@ -8,9 +17,8 @@ int pos = 0;    // variable to store the servo position
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the Servo object
-  Serial.begin(9600);
+  Serial.begin(9600);//for communication
 }
-
 void loop() {
   if (Serial.available() > 0) {
     angle = Serial.read(); // Read the incoming byte
@@ -31,14 +39,14 @@ void loop() {
 // }
 
   if (angle == '1') {
-    for (pos = 0; pos <= 80; pos += 1) { // goes from 0 degrees to 180 degrees
+    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15 ms for the servo to reach the position
   }
       
     } else if (angle == '0') {
-      for (pos = 80; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+      for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15 ms for the servo to reach the position
     }
